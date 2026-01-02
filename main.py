15aa3a983c56b22e414e626b2b1251506b7047fe
Aaditya <desh.aaditya165@gmail.com>
2026-01-03 00:05:44 +0530

    implement model training module with classification metrics and evaluation functionality

--- main.py
+++ main.py
@@ -4,8 +4,9 @@ from networksecurity.logging.logger import logging
 from networksecurity.entity.config_entity import DataIngestionConfig, TrainingPipelineConfig
 from networksecurity.components.data_validation import DataValidation, DataValidationConfig
 import sys
-from networksecurity.entity.config_entity import DataTransformationConfig
+from networksecurity.entity.config_entity import DataTransformationConfig, ModelTrainerConfig
 from networksecurity.components.data_tranformation import DataTransformation
+from networksecurity.components.model_trainer import ModelTrainer
 if __name__ == "__main__":
     try:
         training_pipeline_config=TrainingPipelineConfig()
@@ -26,6 +27,12 @@ if __name__ == "__main__":
         data_transformation_artifact=data_transformation.initiate_data_transformation()
         print(data_transformation_artifact)
         logging.info(f"Data Transformation completed")
+        
+        logging.info("Model training started")
+        model_trainer_config=ModelTrainerConfig(training_pipeline_config)
+        model_trainer=ModelTrainer(model_trainer_config,data_transformation_artifact)
+        model_trainer_artifact=model_trainer.initiate_model_trainer()
+        logging.info(f"Model Trainer completed {model_trainer_artifact}")
 
     except Exception as e:
         raise NetworkSecurityException(e,sys)
